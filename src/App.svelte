d4210493b5ed65dafa355854991425513d588374
matthieuEv <evainmatthieu44@gmail.com>
2024-03-08 22:34:40 +0100

    svelte store working

---
+++ src/App.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { Router, Route, navigate } from "svelte-routing";
+    import Login from "./login.svelte";
+    import Play from "./play.svelte";
+</script>
+
+<main>
+    <Router>
+        <div>
+            <Route path="/" component={Login} />
+            <Route path="/play" component={Play} />
+        </div>
+    </Router>
+</main>
---
+++ src/play.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import { user } from './stores.js';
+    import { get } from 'svelte/store';
+    import { navigate } from 'svelte-routing';
+
+    onMount(() => {
+        const value = get(user);
+        console.log(value);
+    });
+</script>
+
+<main>
+    <h1>Page 2</h1>
+    {#each $user as u, i}
+        <p>{i+1}. {u.username} - {u.score}</p>
+    {/each}
+    <button on:click={() => navigate('/')}>Goto Page 1</button>
+</main>
